9d27b168dc64479cade2efcdad54bd63c47d3203
Mert Metin <necatimertmetin@gmail.com>
2022-10-29 22:36:44 +0300

    Added the files

---
+++ app.js
@@ -0,0 +1,17 @@
+const splash = document.querySelector('.splash');
+document.addEventListener('DOMContentLoaded',(e)=>{
+    setTimeout(()=>{
+        splash.classList.add('display-none');
+    },5500);
+})
+const header = document.querySelector('.header');
+window.onscroll = function(){
+    var top = windows.scrollY;
+    console.log(top);
+    if(top >= 50){
+        header.classList.add('active')
+    }else{
+        header.classList.remove('active');
+    }
+}
+
